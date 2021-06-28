727304057f648f05fa725e20078250e5556cf0df
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-06-28 13:17:20 +0300

    Resolve phone number

--- phone-number/src/main/kotlin/PhoneNumber.kt
+++ phone-number/src/main/kotlin/PhoneNumber.kt
@@ -1,6 +1,13 @@
-class PhoneNumber {
+class PhoneNumber(number: String) {
+    val number = filterNumber(number)
 
-    // TODO: Implement proper constructor
+    private fun filterNumber(number: String): String {
+        val filteredNumber = number.filterNot { it.isWhitespace() }
+                .filter { it.isDigit() }
+                .removePrefix("1")
 
-    val number: String? = TODO("Implement this getter to complete the task")
+        if (filteredNumber.length == 10 && filteredNumber.first() > '1' && filteredNumber[3] > '1') {
+            return filteredNumber
+        } else throw IllegalArgumentException("Incorrect number")
+    }
 }
