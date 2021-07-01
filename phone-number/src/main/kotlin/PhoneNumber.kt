80233ee3f40fe9714a2cc2b4e1dd5a80c920e551
corinaorasan171015 <orasan.corina@agilefreaks.com>
2021-07-01 15:17:30 +0300

    Refactor solution

--- phone-number/src/main/kotlin/PhoneNumber.kt
+++ phone-number/src/main/kotlin/PhoneNumber.kt
@@ -1,13 +1,52 @@
 class PhoneNumber(number: String) {
-    val number = filterNumber(number)
+    val number = showNumberWithoutPrefix(number)
 
-    private fun filterNumber(number: String): String {
-        val filteredNumber = number.filterNot { it.isWhitespace() }
-                .filter { it.isDigit() }
-                .removePrefix("1")
+    private fun String.cleanup(): String {
+        return this.eliminateWhiteSpaces()
+                .filterDigits()
+                .removePrefixOne()
+    }
 
-        if (filteredNumber.length == 10 && filteredNumber.first() > '1' && filteredNumber[3] > '1') {
-            return filteredNumber
-        } else throw IllegalArgumentException("Incorrect number")
+    private fun showNumberWithoutPrefix(number: String): String {
+        return if (number.cleanup()
+                        .isValid()) number.cleanup() else throw IllegalArgumentException()
+    }
+
+    private fun String.isValid(): Boolean {
+
+        val lengthRule = { string: String -> string.isLengthEqualTen() }
+
+        val firstCharacterRule = { string: String -> string.isCharacterGreaterThanOne(0) }
+
+        val fourthCharacterRule = { string: String -> string.isCharacterGreaterThanOne(3) }
+
+        return listOf(firstCharacterRule, fourthCharacterRule, lengthRule).all {
+            it(this)
+        }
+    }
+
+    private fun String.removePrefixOne(): String {
+        return this.removePrefix(AMERICAN_PREFIX)
+    }
+
+    private fun String.eliminateWhiteSpaces(): String {
+        return this.filterNot { it.isWhitespace() }
+    }
+
+    private fun String.filterDigits(): String {
+        return this.filter { it.isDigit() }
+    }
+
+    private fun String.isLengthEqualTen(): Boolean {
+        return this.length == PHONE_NUMBER_LENGTH
+    }
+
+    private fun String.isCharacterGreaterThanOne(index: Int): Boolean {
+        return this[index] > '1'
+    }
+
+    companion object {
+        const val PHONE_NUMBER_LENGTH = 10
+        const val AMERICAN_PREFIX = "1"
     }
 }
